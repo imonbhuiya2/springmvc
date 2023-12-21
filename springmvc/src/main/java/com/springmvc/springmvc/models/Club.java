01208bedf90166243b395f102b1c18a213997090
Md Imon Bhuiya <imonbhuiya2@gmail.com>
2023-12-21 02:24:00 +0100

    updated

--- springmvc/src/main/java/com/springmvc/springmvc/models/Club.java
+++ springmvc/src/main/java/com/springmvc/springmvc/models/Club.java
@@ -1,2 +1,36 @@
-package com.springmvc.springmvc.models;public class Club {
+package com.springmvc.springmvc.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "clubs")
+
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime created;
+    @UpdateTimestamp
+    private LocalDateTime updated;
+
+
+
+
+
 }
